6301e5a4c75e61032c3edc7a35ad1c75c2ce0872
Pablo Palacios <pablo.palacios@piconsulting.com.ar>
2025-10-31 15:06:46 -0300

    update server sse

--- mcp_proyect/server_sse_mcp.py
+++ mcp_proyect/server_sse_mcp.py
@@ -1,11 +1,10 @@
 
 import logging
 from mcp.server.fastmcp import FastMCP
-#from mcp.server.streaming_asgi_transport import StreamingASGITransport
 
-class MCP_SERVER_STREAM:
+class MCP_SERVER_SEE:
     def __init__(self, name_server: str):
-        self.mcp = FastMCP(name=name_server, streamable_http_path="/mcp",stateless_http=True)
+        self.mcp = FastMCP(name=name_server, sse_path="/mcp_sse", stateless_http=True)
 
         list_name = []
        
@@ -37,10 +36,40 @@ class MCP_SERVER_STREAM:
             list_name.append(names)
             return f"Se agrego con exito el nombre: {names}"
         
+        @self.mcp.tool(name="remove_name", description="Remove one name from the list if it exists.")
+        async def remove_name(name: str):
+                if name in list_name:
+                    list_name.remove(name)
+                    return f"Se eliminó el nombre: {name}"
+                else:
+                    return f"El nombre '{name}' no se encuentra en la lista."
+                
+        import random
 
+        @self.mcp.tool(name="pick_random_name", description="Selects a random name from the list.")
+        async def pick_random_name():
+            if not list_name:
+                return "La lista está vacía, agrega nombres primero."
+            return f"El nombre elegido al azar es: {random.choice(list_name)}"
+        
+
+        @self.mcp.tool(name="write_text_file", description="Writes text content into a local file.")
+        async def write_text_file(filename: str, content: str):
+            with open(filename, "w", encoding="utf-8") as f:
+                f.write(content)
+            return f"Archivo '{filename}' creado y guardado exitosamente."
+        
+        @self.mcp.tool(name="read_text_file", description="Reads the content of a local text file.")
+        async def read_text_file(filename: str):
+            try:
+                with open(filename, "r", encoding="utf-8") as f:
+                    content = f.read()
+                return content
+            except FileNotFoundError:
+                return f"El archivo '{filename}' no existe."
 
         
-        @self.mcp.resource(url="config://settings", name="application_settings", description="send infomation to specific settings in json")
+        @self.mcp.resource(uri="config://settings", name="application_settings", description="send infomation to specific settings in json")
         def get_settings() -> str:
             """Get application settings."""
 
@@ -53,12 +82,12 @@ class MCP_SERVER_STREAM:
 
 
     def run(self):
-        self.mcp.run(transport="streamable-http")
+        self.mcp.run(transport="sse")
 
 
 if __name__=="__main__":
-    server = MCP_SERVER_STREAM("SERVER STREAM")
-    logging.info("MCP SERVER UP STREAM!!")
+    server = MCP_SERVER_SEE("SERVER SSE")
+    logging.info("MCP SERVER UP SSE!!")
     server.run()
     
 
